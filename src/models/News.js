965ef0fe6a4f5736e34b790f68eab63f6ff8e837
rodrigo <rodrigoferreira.tico@gmail.com>
2024-07-09 16:29:11 -0300

    feat: News Schema

---
+++ src/models/News.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const NewsSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            require: true
+        },
+        text: {
+            type: String,
+            require: true
+        },
+        banner: {
+            type: String,
+            require: true
+        },
+        createdAt: {
+            type: Date,
+            dafault: Date.now()
+        },
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            require: true
+        },
+        likes: {
+            type: Array,
+            require: true
+        },
+        comments: {
+            type: Array,
+            require: true
+        }
+    }
+);
+
+const News = mongoose.model("News", NewsSchema);
+
+export default News;
